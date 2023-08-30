fcfe1afbb1c4ab1bc841733c48214fe818264fb3
CypherKB <kylianbaeyens1@hotmail.be>
2023-08-30 21:23:29 +0200

    Update Thesis.py

--- Thesis.py
+++ Thesis.py
@@ -1,1 +1,25 @@
 # Masterthesis
+import random
+def initialize_population(initial_size:int,ploidy_number:int,loci_number:int):
+    """_summary_
+
+    Args:
+        initial_size (int): Size of the initial population of individuals to start with
+        ploidy_number (int): Number of chromosome sets per individual
+        loci_number (int): Number of loci per chromosome
+
+    Returns:
+        _type_: _description_
+    """
+    population = []
+    for i in range(0, initial_size):
+        individual = []
+        for i in range(0, ploidy_number):
+            chromosome_set = []
+            for i in range(0, loci_number):
+                chromosome_set.append(random.choice([0,1]))
+            individual.append(chromosome_set)
+        population.append(individual)
+    return population
+
+print(initialize_population(5,2,3))
