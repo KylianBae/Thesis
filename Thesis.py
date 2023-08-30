61856a0ec3ba6f599b3bf3b073a028bb4c1aa518
CypherKB <kylianbaeyens1@hotmail.be>
2023-08-30 21:30:55 +0200

    Update Thesis.py

--- Thesis.py
+++ Thesis.py
@@ -2,14 +2,15 @@
 import random
 def initialize_population(initial_size:int,ploidy_number:int,loci_number:int):
     """_summary_
-
+    Initialize the starting population with a given number of individuals of the population (initial_size), with its ploidy level(ploidy_number)
+    and number of loci per chromosome (loci_number)
     Args:
         initial_size (int): Size of the initial population of individuals to start with
         ploidy_number (int): Number of chromosome sets per individual
         loci_number (int): Number of loci per chromosome
 
     Returns:
-        _type_: _description_
+        List of lists which represents the population with its individuals based on the given parameters
     """
     population = []
     for i in range(0, initial_size):
@@ -22,4 +23,6 @@ def initialize_population(initial_size:int,ploidy_number:int,loci_number:int):
         population.append(individual)
     return population
 
-print(initialize_population(5,2,3))
+print(initialize_population(10,2,4))
+def meisosis(population):
+    
