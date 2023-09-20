834184e79dfddc1a60dbb8dd194239328bb7d278
KylianBae <Kylian.Baeyens@UGent.be>
2023-09-20 21:43:23 +0200

    Update Thesis.py

--- Thesis.py
+++ Thesis.py
@@ -25,7 +25,7 @@ def initialize_population(initial_size:int,ploidy_number:int,loci_number:int):
         population.append(individual)
     return population
 
-def meiosis(arg,v=0.5):
+def meiosis(arg,v=0.3):
     gametes = []
     if len(arg) == 2:
         chromosomes = random.choices([1,2],weights=(1-v,v),k=1)
@@ -49,13 +49,15 @@ def meiosis(arg,v=0.5):
     if len(arg) == 4:
         chromosomes = random.choices([1,2],weights=(1- v,v),k=1)
         if chromosomes[0] == 1:
-            gametes.append(random.sample(arg,1)[0])   
+            poss_gametes = random.sample(arg,2)
+            gametes.append(poss_gametes[0])
+            gametes.append(poss_gametes[1])   
     return gametes
 
 def cytotype_dynamics(initial_population:list,max_generations:int):
     t = 1
     n = len(initial_population)
-    freq_diploid, freq_triploid, freq_tetraploid = [], [], []
+    freq_diploid, freq_triploid, freq_tetraploid = [1000], [0], [0]
     while t < max_generations:
         new_pop = []
         while len(new_pop) < n:
@@ -65,23 +67,23 @@ def cytotype_dynamics(initial_population:list,max_generations:int):
             if first != [] and second != [] and len(first + second) < 5:
                 offspring = first + second
                 new_pop.append(offspring)
-        freq_diploid.append(len([sublist for sublist in new_pop if len(sublist) == 2]))
-        freq_triploid.append(len([sublist for sublist in new_pop if len(sublist) == 3]))
-        freq_tetraploid.append(len([sublist for sublist in new_pop if len(sublist) == 4]))
+        freq_diploid.append(sum(1 for individual in new_pop if len(individual) == 2))
+        freq_triploid.append(sum(1 for individual in new_pop if len(individual) == 3))
+        freq_tetraploid.append(sum(1 for individual in new_pop if len(individual) == 4))
         t += 1
-    return [freq_diploid,freq_triploid,freq_tetraploid]
+    return [new_pop,t,freq_diploid,freq_triploid,freq_tetraploid]
 
-def makeplot(t, data):
+def makeplot(data):
+    t = list(range(1,data[1]+1))
     plt.figure()
-    plt.plot(t,data[0], label= "Frequency diploid")
-    plt.plot(t,data[1], label= "Frequency triploid")
-    plt.plot(t,data[2], label= "Frequency tetraploid")
-    # Add labels and legend
-    plt.xlabel('generation')
-    plt.ylabel('frequency')
-    plt.title('Plot of frequencies of diploids, triploids and tetraploid per generation')
+    plt.plot(t,data[2], label= "Frequency diploid")
+    plt.plot(t,data[3], label= "Frequency triploid")
+    plt.plot(t,data[4], label= "Frequency tetraploid")
+    plt.xlabel("Generation")
+    plt.ylabel("Frequency")
+    plt.title("Plot of frequencies of diploids, triploids and tetraploids per generation")
     plt.legend()
     plt.show()
 
 x = initialize_population(1000,2,2)
-makeplot(100,cytotype_dynamics(x,100))
+print(makeplot(cytotype_dynamics(x,1000000)[0:]))
