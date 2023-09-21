1d3ed5f4e7da00358bdf77c86e01d1ae440f38f8
KylianBae <Kylian.Baeyens@UGent.be>
2023-09-21 17:29:39 +0200

    Update Thesis.py

--- Thesis.py
+++ Thesis.py
@@ -25,10 +25,10 @@ def initialize_population(initial_size:int,ploidy_number:int,loci_number:int):
         population.append(individual)
     return population
 
-def meiosis(arg,v=0.3):
+def meiosis(arg,v):
     gametes = []
-    if len(arg) == 2:
-        chromosomes = random.choices([1,2],weights=(1-v,v),k=1)
+    if len(arg) == 2:  
+        chromosomes = random.choices([1,2],weights=(1-v,v),k=1) # choice for reduced or unreduced gametes
         if chromosomes[0] == 1:
             gametes.append(random.sample(arg,1)[0])
         else:
@@ -47,14 +47,14 @@ def meiosis(arg,v=0.3):
             gametes.append(poss_gametes[1])
             gametes.append(poss_gametes[2])
     if len(arg) == 4:
-        chromosomes = random.choices([1,2],weights=(1- v,v),k=1)
+        chromosomes = random.choices([1,2],weights=(1- v,v),k=1) # choice for diploid gametes or no gametes
         if chromosomes[0] == 1:
             poss_gametes = random.sample(arg,2)
             gametes.append(poss_gametes[0])
             gametes.append(poss_gametes[1])   
     return gametes
 
-def cytotype_dynamics(initial_population:list,max_generations:int):
+def cytotype_dynamics(initial_population:list,max_generations:int,v:float):
     t = 1
     n = len(initial_population)
     freq_diploid, freq_triploid, freq_tetraploid = [1000], [0], [0]
@@ -62,14 +62,14 @@ def cytotype_dynamics(initial_population:list,max_generations:int):
         new_pop = []
         while len(new_pop) < n:
             pair = random.sample(initial_population,2)
-            first = meiosis(pair[0])
-            second = meiosis(pair[1])
+            first = meiosis(pair[0],v)
+            second = meiosis(pair[1],v)
             if first != [] and second != [] and len(first + second) < 5:
                 offspring = first + second
                 new_pop.append(offspring)
-        freq_diploid.append(sum(1 for individual in new_pop if len(individual) == 2))
-        freq_triploid.append(sum(1 for individual in new_pop if len(individual) == 3))
-        freq_tetraploid.append(sum(1 for individual in new_pop if len(individual) == 4))
+        freq_diploid.append(sum(1 for individual in new_pop if len(individual) == 2)) # count how many diploids in each generation
+        freq_triploid.append(sum(1 for individual in new_pop if len(individual) == 3)) # count how many triploids in each generation
+        freq_tetraploid.append(sum(1 for individual in new_pop if len(individual) == 4)) # count how many tetraploids in each generation
         t += 1
     return [new_pop,t,freq_diploid,freq_triploid,freq_tetraploid]
 
@@ -85,5 +85,6 @@ def makeplot(data):
     plt.legend()
     plt.show()
 
+# Example
 x = initialize_population(1000,2,2)
-print(makeplot(cytotype_dynamics(x,1000000)[0:]))
+print(makeplot(cytotype_dynamics(x,1000,0.1)[0:]))
